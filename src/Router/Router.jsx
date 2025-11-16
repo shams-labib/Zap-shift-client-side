bcc58efae1660e81096552ca880030494d44a40d
shams al labib <samslabib19@gmail.com>
2025-11-16 21:11:55 +0600

    implitended react hook form

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -2,6 +2,9 @@ import { createBrowserRouter } from "react-router";
 import RootLayouts from "../Layouts/RootLayouts";
 import Home from "../Pages/Home/Home/Home";
 import Coverage from "../Pages/Coverage/Coverage";
+import AuthLayout from "../Layouts/AuthLayout";
+import Login from "../Pages/Auth/Login/Login";
+import Register from "../Pages/Auth/Register/Register";
 
 export const router = createBrowserRouter([
   {
@@ -16,6 +19,20 @@ export const router = createBrowserRouter([
         path: "/coverage",
         loader: () => fetch("/serviceCenter.json").then((res) => res.json()),
         element: <Coverage></Coverage>,
+      },
+    ],
+  },
+  {
+    path: "/",
+    Component: AuthLayout,
+    children: [
+      {
+        path: "/login",
+        Component: Login,
+      },
+      {
+        path: "/register",
+        Component: Register,
       },
     ],
   },
