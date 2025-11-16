ddeb8b9eb4d3446630aaad79261480b4345c86af
shams al labib <samslabib19@gmail.com>
2025-11-16 12:38:28 +0600

    added

---
+++ src/Pages/Coverage/Coverage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Coverage = () => {
+  return (
+    <div>
+      <h2 className="text-3xl">We are available in 64 districts</h2>
+    </div>
+  );
+};
+
+export default Coverage;
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -9,7 +9,10 @@ const Navbar = () => {
         <NavLink to={""}>Services</NavLink>
       </li>
       <li>
-        <NavLink to={""}>Services</NavLink>
+        <NavLink to={""}>About Us</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/coverage"}>Coverage</NavLink>
       </li>
     </>
   );
